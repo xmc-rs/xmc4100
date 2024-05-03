507bed9ab2c75d348571a739671c7d7332bd9496
Lucas Brendel <lucasbrendel@gmail.com>
2024-05-03 06:22:09 -0500

    Revert "chore: updating svd build process"
    This reverts commit 83895c75b2b866748e974cbf33771426d6e4830b.

--- svd.ps1
+++
@@ -1,7 +0,0 @@
-
-Remove-Item .\src -Recurse -Force
-svd2rust -i .\XMC4100.svd --ident-formats-theme legacy
-New-Item src -ItemType Directory | Out-Null
-form -i .\lib.rs -o .\src
-cargo fmt
-Remove-Item .\lib.rs -Force
---
+++ svd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# if ["$1" -eq "-i"]; then
+#     cargo install -f svd2rust
+#     cargo install -f form
+rm -r src/
+svd2rust -i XMC4100.svd --ident-formats-theme legacy
+mkdir src
+form -i lib.rs -o src/
+cargo fmt
+rm lib.rs
